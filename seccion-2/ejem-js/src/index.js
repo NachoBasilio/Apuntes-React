3a79884112a22fd13c0c5e14fdfcf611529beb6d
IgnaDev <arcortoon@gmail.com>
2022-05-02 19:14:31 -0300

    Hoy arranco el trabajo, supuestametne jaja

--- seccion-2/ejem-js/src/index.js
+++ seccion-2/ejem-js/src/index.js
@@ -1,1 +1,2 @@
-console.log("hola mundo")
+//Las notas que tome en esta parte solo seran de las cosas que me llamen la atencion de la parte de repazo de JS
+//Las backticks las puedo hacer con control + shift + u + 6 + 0  
