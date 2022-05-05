9180f7f273f70b7a23afbfa31212e7ae72875ac8
IgnaDev <arcortoon@gmail.com>
2022-05-05 19:00:54 -0300

    Termine la parte de JS

--- seccion-2/ejem-js/src/index.js
+++ seccion-2/ejem-js/src/index.js
@@ -4,5 +4,19 @@
 //Desestructurar arrays es lo mismo que con los objetos solo que usa [] y respeta el orden, podemos usar , para separar los valores o indicar que nos saltamos algunos valores
 const personas = ['Juan', 'Pedro', 'Maria', 'Trinidad', 'Luis', 'Jorge'];
 //Imaginemos que solo quiero a Trinidad ya que es mi novia
-const [, , , , , nombre] = personas;
+const [, , , nombre] = personas;
 console.log(nombre);
+
+//En cuanto a los async await, es una forma de hacerlo de manera mas sencilla
+const getPokemon = async (numero) => {
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`);
+        const pokemon = await response.json();
+        const url = pokemon.sprites.front_default
+        console.log(url);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+getPokemon(1);
