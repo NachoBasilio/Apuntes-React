081b2a670a52a3739a18afc88e33ff2298232456
IgnaDev <arcortoon@gmail.com>
2022-05-17 14:51:00 -0300

    Aprendi que son todas las cosas que crea react app y ahora vamos a hacer nuestro primer componente

--- seccion-3/counter-app/src/index.js
+++ seccion-3/counter-app/src/index.js
@@ -1,17 +1,11 @@
+//Vamos a usar JSX que es una forma de escribir HTML dentro de Javascript, para usarlo tenemos que importar el modulo de react
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Ahora vamos a importar el ReactDOM para poder renderizar el componente en el DOM
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const saludo = <h1>Hola Mundo</h1>;//Ahora podemos usar JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const divRoot = document.getElementById('root');//Ahora vamos a obtener el elemento del DOM que queremos renderizar
+
+
+ReactDOM.render(saludo, divRoot);//Ahora vamos a renderizar el componente en el DOM
