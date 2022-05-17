7413a87472588a1037a6ec54d0bd6c56ba602b26
IgnaDev <arcortoon@gmail.com>
2022-05-17 17:06:58 -0300

    Vamos a ver como comunicarnos entre componentes

--- seccion-3/counter-app/src/PrimeraApp.js
+++ seccion-3/counter-app/src/PrimeraApp.js
@@ -1,12 +1,21 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 //vamos a crear un componente usando funciones
 
 const PrimeraApp = () => {
-    return <Fragment>
-        <h1>Hola Mundo</h1>
-        <p>Este es un parrafo</p>
-    </Fragment>//fragment es una etiqueta que permite agrupar varios elementos en un solo componente, lo tenemos que importar de la libreria react, tambien podemos usar <> </>.
+    const saludo = 'Hola Mundo';
+    const arregloDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const objetoRandom = {
+        nombre: 'Juan',
+        apellido: 'Perez',
+        edad: '30',
+    }
+
+    return <>
+        <h1> { saludo /* aca podemos poner constantes o variables, no todo pero si muchas cosas pueden ser llamadas de esta manera */ } </h1>
+        <p>{arregloDeNumeros}</p>
+        {/* <pre>{JSON.stringify(objetoRandom, null, 3)}</pre>*/}
+    </>//fragment es una etiqueta que permite agrupar varios elementos en un solo componente, lo tenemos que importar de la libreria react, tambien podemos usar <> </>.
 }
 
 export default PrimeraApp;
