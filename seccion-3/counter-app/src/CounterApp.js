bc37dee7672f86cb997284af7e810620d7265021
IgnaDev <arcortoon@gmail.com>
2022-05-17 20:54:14 -0300

    Segunda prueba

--- seccion-3/counter-app/src/CounterApp.js
+++ seccion-3/counter-app/src/CounterApp.js
@@ -1,18 +1,36 @@
+//rafc, rafce y rafcp son maneras de crear rapido un componente
+
 //importamos react 
-import React from 'react';
+import React, {useState} from 'react';
 import propTypes from 'prop-types';
 
-const CounterApp = ({value}) => {
+
+
+
+const CounterApp = () => {
+
+    //handleAdd
+    const handleAdd = () => {
+        setCounter(counter +1)
+    }
+
+    //useState
+    const [counter, setCounter] = useState(0);
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+            <div className='contenedorDeBotones'>
+                <button onClick={handleAdd}>+1</button>
+                <button onClick={handleAdd}>Reset</button>
+                <button onClick={handleAdd}>-1</button>
+            </div>
+            
         </>
     )
 }
 
-CounterApp.propTypes = {
-    value: propTypes.number.isRequired
-}
+
 
 export default CounterApp;
--- seccion-3/counter-app/src/index.js
+++ seccion-3/counter-app/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 //Ahora vamos a importar el ReactDOM para poder renderizar el componente en el DOM
 import ReactDOM from 'react-dom';
 //Ahora vamos a importar el componente que vamos a crear
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 //Ahora vamos a importar el css
 import './index.css';
@@ -12,4 +12,4 @@ import './index.css';
 const divRoot = document.getElementById('root');//Ahora vamos a obtener el elemento del DOM que queremos renderizar
 
 
-ReactDOM.render(<CounterApp value={15} />, divRoot);//Ahora vamos a renderizar el componente en el DOM usando el metodo render, primero el componente y luego el elemento del DOM
+ReactDOM.render(<CounterApp />, divRoot);//Ahora vamos a renderizar el componente en el DOM usando el metodo render, primero el componente y luego el elemento del DOM
