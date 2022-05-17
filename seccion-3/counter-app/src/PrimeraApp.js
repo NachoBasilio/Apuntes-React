17464c8df873fed065bdae6bb590923bf119871d
IgnaDev <arcortoon@gmail.com>
2022-05-17 16:55:52 -0300

    Vamos avanzando con react <3

---
+++ seccion-3/counter-app/src/PrimeraApp.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+//vamos a crear un componente usando funciones
+
+const PrimeraApp = () => {
+    return <Fragment>
+        <h1>Hola Mundo</h1>
+        <p>Este es un parrafo</p>
+    </Fragment>//fragment es una etiqueta que permite agrupar varios elementos en un solo componente, lo tenemos que importar de la libreria react, tambien podemos usar <> </>.
+}
+
+export default PrimeraApp;
--- seccion-3/counter-app/src/index.js
+++ seccion-3/counter-app/src/index.js
@@ -2,10 +2,13 @@
 import React from 'react';
 //Ahora vamos a importar el ReactDOM para poder renderizar el componente en el DOM
 import ReactDOM from 'react-dom';
+//Ahora vamos a importar el componente que vamos a crear
+import PrimeraApp from './PrimeraApp';
+//Ahora vamos a importar el css
+import './index.css';
 
-const saludo = <h1>Hola Mundo</h1>;//Ahora podemos usar JSX
 
 const divRoot = document.getElementById('root');//Ahora vamos a obtener el elemento del DOM que queremos renderizar
 
 
-ReactDOM.render(saludo, divRoot);//Ahora vamos a renderizar el componente en el DOM
+ReactDOM.render(<PrimeraApp />, divRoot);//Ahora vamos a renderizar el componente en el DOM usando el metodo render, primero el componente y luego el elemento del DOM
