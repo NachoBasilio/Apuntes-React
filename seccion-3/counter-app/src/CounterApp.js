deecf904dc233435b152a2838e5c034a358f7154
IgnaDev <arcortoon@gmail.com>
2022-05-17 22:02:14 -0300

    Actualizado al react 18

--- seccion-3/counter-app/src/CounterApp.js
+++ seccion-3/counter-app/src/CounterApp.js
@@ -6,31 +6,37 @@ import propTypes from 'prop-types';
 
 
 
-
-const CounterApp = () => {
+const CounterApp = ({value = 0}) => {
 
     //handleAdd
     const handleAdd = () => {
-        setCounter(counter +1)
+        setCounter( counter + 1 )
+    }
+    const handleRemove = () => {
+        setCounter( counter - 1 )
+    }
+    const handleReset = () => {
+        setCounter( value )
     }
 
     //useState
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{counter}</h2>
             <div className='contenedorDeBotones'>
+                <button onClick={handleRemove}>-1</button>  
+                <button onClick={handleReset}>Reset</button>
                 <button onClick={handleAdd}>+1</button>
-                <button onClick={handleAdd}>Reset</button>
-                <button onClick={handleAdd}>-1</button>
             </div>
-            
         </>
     )
 }
 
-
+propTypes.CounterApp = {
+    value: propTypes.number
+}
 
 export default CounterApp;
