8738dd5dce809d2d2b9e4e76d8562997e115ac7d
IgnaDev <arcortoon@gmail.com>
2022-06-01 15:11:12 -0300

    Vamos a ver los hooks

--- EscuelaDevRock/PrimerosCapitulos/queridojonny/src/components/primerComponente.js
+++ EscuelaDevRock/PrimerosCapitulos/queridojonny/src/components/primerComponente.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import '../assets/css/saludo.css'
 
-const MiPrimerComponenteConJonny = () => {
-    let nombre = "Jonny";
+const MiPrimerComponenteConBootstrap = () => {
+    let nombre = "Nacho";
     return (
-        <div className="saludo">
-            <h1>Hola {nombre}</h1>
+        <div className="saludo">    
+            <h1>Esto es un super ejemplo de que pude installar bootstrap <span class="badge bg-secondary">{nombre}</span></h1>
+            <button type="button" class="btn btn-primary position-relative">
+                    Inbox
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                99+
+                <span class="visually-hidden">unread messages</span>
+            </span>
+            </button>
         </div>
     )
 }
 
-export default MiPrimerComponenteConJonny;
+export default MiPrimerComponenteConBootstrap;
--- EscuelaDevRock/PrimerosCapitulos/queridojonny/src/containers/App.js
+++ EscuelaDevRock/PrimerosCapitulos/queridojonny/src/containers/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import MiPrimerComponenteConJonny from "../components/primerComponente";
+import MiPrimerComponenteConBootstrap from "../components/primerComponente";
 
 
 const App = () => {
     return (
-        <div className="App">
-        <MiPrimerComponenteConJonny/>
-        </div>
+    <div className="App">
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous"
+        />
+        <MiPrimerComponenteConBootstrap/>
+    </div>
     );
 }
 
