619359a3db3f2e4fd023829f97c89a517bfa9bb8
IgnaDev <arcortoon@gmail.com>
2022-07-09 16:05:25 -0300

    Hoy estuve escuchando la tutoria

--- OpenBootCamp/hola-mundo/src/App.js
+++ OpenBootCamp/hola-mundo/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 // import TaskList from './components/container/task_list';
 
 import Ejemplo3 from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 // import Greeting from './components/pure/greeting';
 // import GreetingF from './components/pure/greetingF';
 
 function App() {
   return (
-    <Ejemplo3></Ejemplo3>
+    <div>
+      <Ejemplo3></Ejemplo3>
+      <Ejemplo4>
+        <p>Nacho</p>
+      </Ejemplo4>
+    </div>
   );
 }
 
