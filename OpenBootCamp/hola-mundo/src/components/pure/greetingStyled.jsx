7ee0e478665f2efe9f1d3e9a4c30c54089d81f54
IgnaDev <arcortoon@gmail.com>
2022-10-11 16:30:45 -0300

    Arrancando con el curso de platzi

---
+++ OpenBootCamp/hola-mundo/src/components/pure/greetingStyled.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const loggedStyle = {
+    color: 'blue'
+}
+
+const unLoggedStyle = {
+    color: 'red',
+    fontWeight: 'bold'
+}
+
+const GreetingStyled = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default GreetingStyled;
