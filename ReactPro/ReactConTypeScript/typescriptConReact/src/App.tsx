69e00b5a7008eeb1b6f5824e7c086ec607dbbc1c
IgnaDev <arcortoon@gmail.com>
2023-10-31 20:04:49 -0300

    Prepare todo para comenzar

---
+++ ReactPro/ReactConTypeScript/typescriptConReact/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl">Hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ ReactPro/ReactConTypeScript/typescriptConReact/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(//El signo de exclamación es para decirle a typescript que no se preocupe que el elemento no es nulo
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
