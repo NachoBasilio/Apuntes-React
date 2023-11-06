bce6d2d29e4465f080c7af84afc2b277e06ef886
IgnaDev <arcortoon@gmail.com>
2023-11-06 19:54:43 -0300

    useRef

--- ReactPro/TypeScripConReact/src/bases/CounterEffect.tsx
+++ ReactPro/TypeScripConReact/src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import {gsap} from "gsap"
 
 const MAXIMUN_COUNTE = 10;
@@ -6,6 +6,7 @@ const MAXIMUN_COUNTE = 10;
 
 export const CounterEffect = () => {
     const [contador, setContador] = useState<number>(5)
+    const counterElemnt = useRef<HTMLHeadingElement>(null)
 
     const handlerClick = (numero:number)=>{
         setContador(prev => Math.min(prev + numero, MAXIMUN_COUNTE))
@@ -14,11 +15,10 @@ export const CounterEffect = () => {
     useEffect(() => {
         if(contador >= MAXIMUN_COUNTE){
             setContador(10)
-            gsap.to('.contador', {y: -10, duration:0.5, ease:""})
+            gsap.to(counterElemnt.current, {y: -10, duration:0.5, ease:""})
             .then(()=>{
-                gsap.to('.contador', {y: 0, duration:0.5, ease:"bounce.out"})
+                gsap.to(counterElemnt.current, {y: 0, duration:0.5, ease:"bounce.out"})
             })
-            
             
             
         }
@@ -30,7 +30,7 @@ export const CounterEffect = () => {
   return (
     <>
         <h1 className="text-2xl mt-6">CounterEffect:</h1>
-        <h2 className="contador text-2xl">{contador}</h2>
+        <h2 ref={counterElemnt} className="text-2xl">{contador}</h2>
 
         <button
         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
