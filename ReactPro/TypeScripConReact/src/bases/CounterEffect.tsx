af1e05079c8ad40e8a1c4bb7f8a5cde292c66a7f
IgnaDev <arcortoon@gmail.com>
2023-11-06 19:36:02 -0300

    Agregando animaciones

--- ReactPro/TypeScripConReact/src/bases/CounterEffect.tsx
+++ ReactPro/TypeScripConReact/src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import {gsap} from "gsap"
 
 const MAXIMUN_COUNTE = 10;
 
@@ -7,19 +8,29 @@ export const CounterEffect = () => {
     const [contador, setContador] = useState<number>(5)
 
     const handlerClick = (numero:number)=>{
-        setContador(contador + numero)
+        setContador(prev => Math.min(prev + numero, MAXIMUN_COUNTE))
     }
 
     useEffect(() => {
         if(contador >= MAXIMUN_COUNTE){
             setContador(10)
+            gsap.to('.contador', {y: -10, duration:0.5, ease:""})
+            .then(()=>{
+                gsap.to('.contador', {y: 0, duration:0.5, ease:"bounce.out"})
+            })
+            
+            
+            
         }
+
+
     }, [contador])
     
 
   return (
     <>
-        <h1 className="text-2xl my-6">CounterEffect: {contador} {contador >= MAXIMUN_COUNTE && "Te pasaste del maximo"}</h1>
+        <h1 className="text-2xl mt-6">CounterEffect:</h1>
+        <h2 className="contador text-2xl">{contador}</h2>
 
         <button
         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
